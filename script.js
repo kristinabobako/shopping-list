26a0633867ed19af8863283a23a3023fcce16a08
kristinabobako <kristinabobako@mail.ru>
2023-12-22 17:38:22 +0300

    Добавление элементов списка по нажатию на Enter

--- script.js
+++ script.js
@@ -7,3 +7,33 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+//поле ввода списка покупок
+const submitFiled = document.querySelector('#input');
+//место, где будет храниться список покупок
+const container = document.querySelector('#items');
+
+function addShopingList() {
+    //здесь будет храниться список, кот. ввел пользователь в input
+    const shoppingListUser = submitFiled.value;
+
+    const listItem = document.createElement('div');
+    listItem.classList.add('item');
+    listItem.textContent = shoppingListUser;
+
+    if (shoppingListUser != '') {
+        container.append(listItem);
+    }
+
+    submitFiled.value = '';
+}
+
+function sendByKey(event) {
+    if (event.key == 'Enter') {
+        addShopingList();
+    }
+}
+
+submitFiled.addEventListener('keydown', sendByKey);
+
+
